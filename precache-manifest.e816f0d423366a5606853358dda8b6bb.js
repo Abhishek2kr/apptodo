3b7c0282b7c328fb03393bab8c281c0eeaa9dbae
Abhishek Kumar <abhishekkumar@latitudefintech.com>
2019-07-26 15:57:46 +0530

    Updates

---
+++ precache-manifest.e816f0d423366a5606853358dda8b6bb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9290333c068d8fd7f51b4f643bf513e2",
+    "url": "/apptodo/index.html"
+  },
+  {
+    "revision": "8d329cf557245e46ffff",
+    "url": "/apptodo/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "b03856cdad7276a5e8b3",
+    "url": "/apptodo/static/js/2.f649222c.chunk.js"
+  },
+  {
+    "revision": "8d329cf557245e46ffff",
+    "url": "/apptodo/static/js/main.8519f774.chunk.js"
+  },
+  {
+    "revision": "7a8b518d112f39e53ca5",
+    "url": "/apptodo/static/js/runtime~main.6ea82fc7.js"
+  }
+]);
